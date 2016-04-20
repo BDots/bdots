336b890e09741fbcd89edb8a76a0cecdaf038cb5
Scott-Tomato <scott.shixh@gmail.com>
2016-04-20 17:56:37 +0800

    Added files via upload

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,8 +1,24 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Button } from "react-bootstrap";
 
 export class HelloComponent extends React.Component<any, any> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        var helloword = new HelloWord(this.props.framework);
+        //return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return <Button bsStyle="primary" bsSize="large" block>{helloword.helloword()}</Button>;
     }
+}
+
+
+class HelloWord{
+  Message : string;
+
+  constructor (message : string){
+    this.Message = message;
+  }
+
+  helloword(){
+      return "Hello Word " + this.Message;
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
+
 module.exports = {
-    entry: "./src/index.tsx",
+    entry: "./src/view/index.tsx",
     output: {
+        //path: config.devPath + '/dist',
         filename: "./dist/bundle.js",
     },
 
@@ -14,6 +16,13 @@ module.exports = {
 
     module: {
         loaders: [
+            //{ test: /\.css$/, loader: 'style-loader!css-loader' },
+            //.js 文件使用 jsx-loader 来编译处理
+            //{ test: /\.js$/, loader: 'jsx-loader?harmony' },
+            //.scss 文件使用 style-loader、css-loader 和 sass-loader 来编译处理
+            //{ test: /\.scss$/, loader: 'style!css!sass?sourceMap'},
+            //图片文件使用 url-loader 来处理，小于8kb的直接转为base64
+            //{ test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'},
             // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
             { test: /\.tsx?$/, loader: "ts-loader" }
         ],
